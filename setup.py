d3cd8d4475271653bd69d3da3ffc8e9a9fabd970
Ben Cordero <bmc@linux.com>
2012-06-17 13:39:25 +0100

    openshift doesn't respect distutils :(

--- setup.py
+++ setup.py
@@ -1,8 +1,30 @@
-from distribute.core import setup
+from setuptools import setup
+
+def parse_requirements(file_name):
+    requirements = []
+    for line in open(file_name, 'r').read().split('\n'):
+        if re.match(r'(\s*#)|(\s*$)', line):
+            continue
+        if re.match(r'\s*-e\s+', line):
+            requirements.append(re.sub(r'\s*-e\s+.*#egg=(.*)$', r'\1', line))
+        elif re.match(r'\s*-f\s+', line):
+            pass
+        else:
+            requirements.append(line)
+    return requirements
+
+def parse_dependency_links(file_name):
+    dependency_links = []
+    for line in open(file_name, 'r').read().split('\n'):
+        if re.match(r'\s*-[ef]\s+', line):
+            dependency_links.append(re.sub(r'\s*-[ef]\s+', '', line))
+    return dependency_links
 
 setup(name='hb',
-      version='0.0.1',
-      description='Django Server Heartbeat App',
-      author='Ben Cordero',
-      author_email='bmc@linux.com',
-     )
+    version='0.0.1',
+    description='Django Server Heartbeat App',
+    author='Ben Cordero',
+    author_email='bmc@linux.com',
+    install_requires = parse_requirements('requirements.txt'),
+    dependency_links = parse_dependency_links('requirements.txt'),    
+)
