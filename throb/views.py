9fc983184910f6c5f28ab550b545a334e46f6f15
Ben Cordero <bmc@linux.com>
2012-06-17 14:23:29 +0100

    openshift doesn't set all REMOTE_* variables

--- throb/views.py
+++ throb/views.py
@@ -8,16 +8,19 @@ def ping(request):
     import socket
     now = datetime.datetime.now()
     me = "%s(%s)" % (
-        request.META['SERVER_NAME'],
+        request.META.get('SERVER_NAME', ''),
         "%s:%s"%(
-            socket.gethostbyname(
-                request.META['SERVER_NAME']),
-                request.META['SERVER_PORT']))
+            socket.gethostbyname(request.META.get('SERVER_ADDR', '')),
+            request.META.get('SERVER_PORT', '')
+        )
+    )
     you = "%s(%s)" % (
-        request.META['REMOTE_HOST'],
-        request.META['REMOTE_ADDR'])
+        request.META.get('REMOTE_HOST', ''),
+        "%s:%s"%(
+            request.META.get('REMOTE_ADDR',''),
+            request.META.get('REMOTE_PORT', '')
+        )
+    )
     html = "%s: %s %s" % (
-        now,
-        me,
-        you)
+        now, me, you)
     return HttpResponse(html)
